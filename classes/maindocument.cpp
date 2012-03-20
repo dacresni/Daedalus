3fb07d8bf88ad79a9362907af651407206fa8371
Nehemiah I. Dacres <vivacarlie@gmail.com>
2012-03-20 18:42:08 -0500

    removed unnecesary document class models, This isn't cocoa

--- classes/maindocument.cpp
+++
@@ -1,8 +0,0 @@
-#include "maindocument.h"
-
-MainDocument::MainDocument(QObject *parent) :
-    QObject(parent)
-{
-     *pageList = new QTreeWidgit(this);
-}
-MainDocument::NewSheet(QTreeWidgitItem *parent ) :
